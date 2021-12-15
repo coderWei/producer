e44a93ec89bd34792d0256ffaa1ed7868de5c978
coderWei <1447234286@qq.com>
2021-12-15 11:25:11 +0800

    实体类上字段注解当MP开启驼峰转换的时候，不需要填入value值 重写方法中指定字段值

--- src/main/java/com/tiantong/producer/component/MyMetaObjectHandler.java
+++ src/main/java/com/tiantong/producer/component/MyMetaObjectHandler.java
@@ -11,6 +11,10 @@ import java.time.LocalDateTime;
 
 /**
  * mybatis-plus 自动填充时间方法
+ *
+ * 自动填充字段值策略测试
+ * 通道塞值和默认方法塞值
+ *
  * @author wei
  */
 @Component
@@ -21,16 +25,16 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         log.info("start insert fill ....");
-        this.strictInsertFill(metaObject, "createTs", LocalDateTime.class, LocalDateTime.now());
-        this.strictInsertFill(metaObject, "lastUpdTs", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "create_ts", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "last_upd_ts", LocalDateTime.class, LocalDateTime.now());
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         log.info("start update fill ....");
-        this.strictUpdateFill(metaObject, "createTs", LocalDateTime::now, LocalDateTime.class);
+        this.strictUpdateFill(metaObject, "create_ts", LocalDateTime::now, LocalDateTime.class);
         // 默认提供的strictUpdateFill为有值不覆盖， gmtModified需要覆盖，利用通用塞值的方法填充
-        this.setFieldValByName("lastUpdTs", LocalDateTime.now(), metaObject);
+        this.setFieldValByName("last_upd_ts", LocalDateTime.now(), metaObject);
     }
 
 }
