9e44a1d4c07fbf0bba71fb7eb7a2f853f958b20e
coderWei <1447234286@qq.com>
2021-12-15 10:51:40 +0800

    MP自动填充策略：实体对象必须注解表字段，当表字段不匹配或识别不到时，取消自动填充；其次，填充数据必须继承框架的自动填充类，重写自动填充规则，保证填充数据符合要求

--- src/main/java/com/tiantong/producer/bean/SysUserBean.java
+++ src/main/java/com/tiantong/producer/bean/SysUserBean.java
@@ -83,14 +83,14 @@ public class SysUserBean implements Serializable {
     /**
      * 创建时间
      */
-    @TableField(fill = FieldFill.INSERT)
+    @TableField(value = "create_ts",fill = FieldFill.INSERT)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTs;
 
     /**
      * 修改时间
      */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @TableField(value = "last_upd_ts",fill = FieldFill.INSERT_UPDATE)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Date lastUpdTs;
 
